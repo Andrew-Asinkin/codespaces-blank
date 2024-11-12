949a293bf23199bc76ea2b835a195abe0e65ff31
Andrew-Asinkin <okamenelyi1@gmail.com>
2024-11-12 06:29:57 +0500

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -14,17 +14,8 @@ def test_first_test():
     # assert response.json() == {"msg": "Hello World"}
 
 
-# def test_create_reciple() -> None:
-#     headers = {"Content-Type": "application/json"}
-#     user_data = {"name": "Никита", "views": 2, "time": 9}
-#     response = client.post("/recipe", headers=headers, json=user_data)
-#     print(f'response.json = {response.json()}')
-
-#     assert response.status_code == 200
-
-
 def test_read_main():
-    response = client.get("/recipe")
+    response = client.get("/recipe/")
     print("begin response")
     print(response)
     print(f'response.json = {response.json()}')
@@ -32,7 +23,7 @@ def test_read_main():
     assert response.status_code == 200
 
 
-def test_read_main2():
+def test_read_main_one():
     response = client.get("/recipe/1")
     print("begin response")
     print(response)
@@ -42,7 +33,7 @@ def test_read_main2():
     assert response.json()['id'] == 1
 
 
-def test_create_reciple2() -> None:
+def test_create_reciple() -> None:
     user_data = {"name": "Никита", "views": 2, "time": 9}
     response = client.post("/recipe/", headers={"Content-Type": "application/json"}, json=user_data)
     print(f'response.json = {response.json()}')
