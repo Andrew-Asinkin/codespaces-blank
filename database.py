b764fb9700838c2ac49196d4d127337050c72393
Andrew-Asinkin <okamenelyi1@gmail.com>
2024-11-13 06:22:03 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,30 @@
-
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_scoped_session, async_sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from asyncio import current_task
 from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+async_session = sessionmaker(engine,
+                             expire_on_commit=False, class_=AsyncSession)
 
 session = async_session()
 Base = declarative_base()
 session_maker = async_sessionmaker(
-            engine, autocommit=False, autoflush=False, expire_on_commit=False, class_=AsyncSession
-        )
+    engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
 my_fast_session = async_scoped_session(session_maker, scopefunc=current_task)
 
 
