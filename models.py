e65ccabc5ccf26436ed8b115538eb99b10cf32a3
Andrew-Asinkin <okamenelyi1@gmail.com>
2024-11-13 07:03:38 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
 
 from database import Base
 
 
 class Recipe(Base):
-    __tablename__ = 'Recipes'
-    __table_args__ = {'extend_existing': True}
+    __tablename__ = "Recipes"
+    __table_args__ = {"extend_existing": True}
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     views = Column(Integer, index=True)
